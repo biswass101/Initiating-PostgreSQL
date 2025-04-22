816463acc22f1d148f5542893431e1202e6b9cad
biswass101 <biswassnaeemcse@gmail.com>
2025-04-22 16:34:40 +0600

    basic's of postgreSql added

--- basic.sql
+++ basic.sql
@@ -1,4 +1,3 @@
--- version viewing for postgres
 select version()
 
 -- table creating
@@ -25,3 +24,65 @@ insert into cars (brand, model, year) values
 SELECT brand, year from cars;
 -- returning all columns
 SELECT * FROM cars;
+
+-- adding column
+ALTER TABLE cars
+ADD color VARCHAR(50);
+
+-- update a existing records
+UPDATE cars
+SET color = 'red'
+WHERE brand = 'Volvo';
+
+SELECT * FROM cars;
+
+UPDATE cars
+set color = 'pink'
+where model = 'M1';
+
+select * from cars;
+-- values of column will be same
+update cars
+set color = 'red';
+
+select * from cars;
+
+-- updating multiple columns
+update cars
+set color = 'yello', year = '2025'
+where brand = 'Toyota';
+
+select * from cars;
+
+--alter column
+-- changing column data type
+-- q1
+alter table cars
+alter column year type varchar(4);
+
+select * from cars;
+
+-- q2
+alter table cars
+alter column color type varchar(55);
+
+select * from cars;
+
+-- Drop Column
+alter table cars
+drop column color;
+
+select * from cars;
+
+-- Delete Recodrs(Note: not specifying where can delete all records)
+delete from cars
+where brand = 'Volvo';
+-- delete all recors method - 1
+delete from cars;
+-- delete all records mewthod -2 
+truncate table cars
+select * from cars
+
+-- Delete table
+drop table cars
+select * from cars
